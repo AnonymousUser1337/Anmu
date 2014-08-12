da32ff4a8b58e0a37f72f8b886556dce0ff08de2
Yeshua Colon <yeshuax17@gmail.com>
2014-08-12 18:53:38 -0400

    Print to the Username and directory

--- Kernel/kernel.cpp
+++ Kernel/kernel.cpp
@@ -1,19 +1,15 @@
-
-
+#include "stdio.h"
 
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kMain. */
 #endif
-#define VIDEO_MEM 0xB8000
 void kMain(void)
 {	
 
-	char * s;
-	//prints an A to the top left of the screen
-	s	= ( char*)VIDEO_MEM;
-	s[0] = 'A';
-	s[1] = 0x03//color attribute
-	
-	
-	
+	int Color = LIGHT_BLUE;
+	int Color2 = LIGHT_RED;
+	kPrintf("Anmu@Computer", Color);
+	kPrintf(" ~/root #\n", Color2);
 }
+
+
