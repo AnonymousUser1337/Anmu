98f702477e6f62911c2a5f51664fbbcfab8d7df0
Yeshua Colon <yeshuax17@gmail.com>
2014-08-12 15:34:25 -0400

    Update kernel.cpp

--- Kernel/kernel.cpp
+++ Kernel/kernel.cpp
@@ -1,9 +1,19 @@
+
+
+
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kMain. */
 #endif
+#define VIDEO_MEM 0xB8000
 void kMain(void)
-{
-	while(1 == 1); //infinite loop
+{	
+
+	char * s;
+	;prints an A to the top left of the screen
+	s	= ( char*)VIDEO_MEM;
+	s[0] = 'A';
+	s[1] = 0x03;color attribute
+	
 	
 	
 }
