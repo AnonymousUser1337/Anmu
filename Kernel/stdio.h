69a136f47e7150ec0b2f0323c6850d3d381c452d
Yeshua Colon <yeshuax17@gmail.com>
2014-08-13 18:11:24 -0400

    Update stdio.h

--- Kernel/stdio.h
+++ Kernel/stdio.h
@@ -39,20 +39,15 @@ unsigned int y_pos = 0;
 unsigned const int Cols = 80;
 
 
-inline void itoa(int, int);
+inline void itoa(int, int, int);
 inline void putch(unsigned char c,int color );
-inline void htoa(int n, int color);
-
-
-
-
 inline void kPrintf(const char *s,int color, ...)
 {
 	
 	va_list list;
     va_start( list, color );
 	int j = 0;
-	location =  VIDEO_MEM+(((Cols*2) * y_pos )+ (x_pos * 2));//calculate the location
+	location =  VIDEO_MEM+(((Cols*2) * y_pos )+ (x_pos * 2));
 	for(int i = 0;i< strlen_Const(s)*2;i+=2)
 	{
 		x_pos++;
@@ -68,28 +63,29 @@ inline void kPrintf(const char *s,int color, ...)
 		}
 		if(s[j] == '%')
 		{
-			
+			x_pos--;
 			switch(s[j+1])//find what format it is in 
 			{
 				case 'i':
 				case 'd':
-					itoa(va_arg(list, int), color);
+					
+					itoa(va_arg(list, int), color, 10);
 				break;
 				case 's':
-				x_pos-=1;//set it to correct location 
+					 
 					kPrintf(va_arg(list,const char*), color);//print out the string
 				break;
 				case 'c':
-				putch((unsigned char)va_arg(list, int), color);//print out char 
+					
+					putch((unsigned char)va_arg(list, int), color);//print out char 
 				break;
-				
-				case 'X':
 				case 'x':
-					htoa(va_arg(list,int),color);
+					
+					kPrintf("0x",color);
+					itoa(va_arg(list,int), color, 16);
 				break;
 			}
-			x_pos++;//increment x
-			j+=2;//skip the format e.g %d, %c
+			j+=2;
 		}
 		
 		else// if no format detected just print char
@@ -105,100 +101,45 @@ inline void kPrintf(const char *s,int color, ...)
 	va_end(list);
 	
 }
-inline void itoa(int n, int color)
+
+inline void itoa(int n, int color,int base)
 {
 	int remainder = 0;
 	int i = 0;
-
 	char *s = 0;
-	while(n>=1)
+	
+	location =  VIDEO_MEM+(((Cols*2) * y_pos )+ (x_pos * 2));
+	while(n != 0)
 	{
 		
-		remainder = n%10;
-		n = n/10;
-		s[i] = remainder + 0x30;
+		remainder = n%base;
+		n/=base;
+		
+		s[i] = (remainder > 9)? (remainder-10) + 'A' : remainder + '0';
 		i++;
 	}
-	int j = i - 1;
-	x_pos--;
-	location =  VIDEO_MEM+(((Cols*2) * y_pos )+ (x_pos * 2));
-	for(int i = 0;i< strlen(s)*2;i+=2)
+	int j = strlen(s) - 1;
+	unsigned char * c = (unsigned char*)location;
+	for(i = 0;i< strlen(s)*2;i+=2)
 	{
-		unsigned char *c = (unsigned char*)location;
+		
 		c[i] = s[j];
 		c[i+1] = color;
 		j--;
+		x_pos++;
 	}
+	
+
 	
 	
 }
 inline void putch(unsigned char c,int color )
 {
-	//write one letter to the screen
 	location =  VIDEO_MEM+(((Cols*2) * y_pos )+ (x_pos * 2));
-	unsigned char * s = (unsigned char*)location - 2;
+	unsigned char * s = (unsigned char*)location;
 	s[0] = c;
 	s[1] = color;
 }
 
-inline void htoa(int n, int color)
-{
-	int remainder = 0;
-	int i = 0;
 
-	char *s = 0;
-	while(n>=1)
-	{
-		
-		remainder = n%16;//get the remainder
-		n = n/16;//divide by 10 to go through the while loop
-		
-		s[i] = remainder;//convert to ascii and store in s
-		i++;//get ready for the next number
-	}
-	int j = i - 1;//set j to the end of the string -1
-	x_pos--;
-	kPrintf("0x0",color);
-	location =  VIDEO_MEM+(((Cols*2) * y_pos )+ (x_pos * 2));//calculate the new location
-	for(int i = 0;i< strlen(s)*2;i+=2)
-	{
-		unsigned char *c = (unsigned char*)location;//set the location
-		//write it to the screen
-		if(s[j] >=0 && s[j] <10)
-		{
-			c[i] = s[j] + 0x30;
-			c[i+1] = color;
-		}
-		else
-		{
-			switch(s[j])
-			{
-				case 10:
-				putch('A',color);
-				break;
-				case 11:
-				putch('B',color);
-				break;
-				case 12:
-				putch('C',color);
-				break;
-				case 13:
-				putch('D',color);
-				break;
-				case 14:
-				putch('E',color);
-				break;
-				case 15:
-				putch('F',color);
-				break;
-				
-				
-			}
-
-		
-		}
-		j--;
-	}
-}
-	
 
