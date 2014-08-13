e3d33160221afba34a381d79e3940d33ad764a52
Yeshua Colon <yeshuax17@gmail.com>
2014-08-13 13:21:34 -0400

    kPrintf can now handle printing integers and strings

--- Kernel/stdio.h
+++ Kernel/stdio.h
@@ -32,26 +32,27 @@
 //14 - Light Brown
 //15 - White
 #define VIDEO_MEM 0xB8000
+#include <stdarg.h>
 unsigned int location = VIDEO_MEM;
 unsigned int x_pos = 0;
 unsigned int y_pos = 0;
 unsigned const int Cols = 80;
 
 
-
-
-
-inline void kPrintf(const char *s, int color)
+inline void itoa(int, int);
+inline void putch(unsigned char c,int color );
+inline void kPrintf(const char *s,int color, ...)
 {
 	
-	
+	va_list list;
+    va_start( list, color );
 	int j = 0;
 	location =  VIDEO_MEM+(((Cols*2) * y_pos )+ (x_pos * 2));
 	for(int i = 0;i< strlen_Const(s)*2;i+=2)
 	{
 		x_pos++;
 		
-		if(s[j] == '\n' || x_pos == Cols || s[j] == '\r')
+		if(s[j] == '\n' || x_pos == Cols)
 		{
 			x_pos = 0;//
 			y_pos++;//go to the next line
@@ -60,16 +61,64 @@ inline void kPrintf(const char *s, int color)
 			
 			j++;//skip the new line character
 		}
+		if(s[j] == '%')
+		{
+			
+			switch(s[j+1])//find what format it is in 
+			{
+				case 'i':
+				case 'd':
+					itoa(va_arg(list, int), color);
+				break;
+				case 's':
+				x_pos-=1;//set it to correct location 
+					kPrintf(va_arg(list,const char*), color);//print out the string
+				break;
+				case 'c':
+				putch((unsigned char)va_arg(list, int), color);//print out char 
+				break;
+			}
+			j+=2;
+		}
 		
+		else// if no format detected just print char
+		{	//Write Letter to Video Memory 
+			unsigned char * c = (unsigned char*)location;
+			c[i] = s[j];
+			c[i+1] = color; 
+			j++;
 		
-		//Write Letter to Video Memory 
-		unsigned char * c = (unsigned char*)location;
-		c[i] = s[j];
-		c[i+1] = color; 
-		j++;
-		
-		
+		}
 		
 	}
+	va_end(list);
 	
 }
+inline void itoa(int n, int color)
+{
+	int remainder = 0;
+	int i = 0;
+	location =  VIDEO_MEM+(((Cols*2) * y_pos )+ (x_pos * 2));
+	while(n!=0)
+	{
+		
+		remainder = n%10;
+		n = n/10;
+		unsigned char * c = (unsigned char*)location - 2;
+		c[i] = remainder + 0x30;
+		c[i+1] = color;
+		i+=2;
+	}
+	
+	
+}
+inline void putch(unsigned char c,int color )
+{
+	location =  VIDEO_MEM+(((Cols*2) * y_pos )+ (x_pos * 2));
+	unsigned char * s = (unsigned char*)location - 2;
+	s[0] = c;
+	s[1] = color;
+}
+
+
+
