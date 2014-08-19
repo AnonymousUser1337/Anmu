deb5be0b71d6161ad7007b5780d53f08fc5dfa3e
Yeshua Colon <yeshuax17@gmail.com>
2014-08-19 00:39:16 -0400

    Create MAKE.bat

---
+++ MAKE.bat
@@ -0,0 +1,32 @@
+@echo off
+cd Boot
+nasm Stage1.asm -o Stage1.bin
+nasm -f elf64 Stage2.asm -o Stage2.o
+copy Stage1.bin "C:\cygwin64\home\Anonymous\Zeroth-OS"
+copy Stage2.o "C:\cygwin64\home\Anonymous\Zeroth-OS"
+del Stage1.bin
+del Stage2.o
+cd C:\cygwin64\home\Anonymous\Zeroth-OS
+cd kernel
+x86_64-elf-g++ -fno-exceptions -fno-rtti -ffreestanding -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow -c -o kernel.o kernel.cpp
+x86_64-elf-g++ -fno-exceptions -fno-rtti -ffreestanding -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow -c -o TextTerminal.o TextTerminal.cpp
+x86_64-elf-g++ -fno-exceptions -fno-rtti -ffreestanding -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow -c -o HAL.o HAL.cpp
+copy kernel.o C:\cygwin64\home\Anonymous\Zeroth-OS
+copy TextTerminal.o C:\cygwin64\home\Anonymous\Zeroth-OS
+copy HAL.o C:\cygwin64\home\Anonymous\Zeroth-OS
+del kernel.o
+del TextTerminal.o
+del HAL.o
+cd C:\cygwin64\home\Anonymous\Zeroth-OS
+x86_64-elf-ld -T linkerscript.ld -o MyOS.bin Stage2.o kernel.o TextTerminal.o HAL.o -nostdlib
+x86_64-elf-objcopy -S -O binary MyOS.bin Anmu.bin
+del MyOS.bin
+copy Stage1.bin Root
+copy Anmu.bin Root
+mkisofs -b Stage1.bin -no-emul-boot -boot-info-table -o BootLoader.iso ./Root
+del Stage1.bin
+del Stage2.o
+del Anmu.bin
+del kernel.o
+del TextTerminal.o
+del HAL.o
