ef70fa0d159cae511573b840a5cb1ec0da5bf89a
Yeshua Colon <yeshuax17@gmail.com>
2014-08-23 23:07:28 -0400

    Update HAL.h

--- Kernel/HAL.h
+++ Kernel/HAL.h
@@ -5,3 +5,5 @@ ASM_LINK void outb(unsigned short port, unsigned char value);
 ASM_LINK unsigned char inb(unsigned short port);
 ASM_LINK void pushall();
 ASM_LINK void popall();
+ASM_LINK void enable_Ints();
+ASM_LINK void disable_Ints();
