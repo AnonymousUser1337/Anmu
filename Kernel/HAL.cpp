3d283d41d2d67986681e81ca4ccfeb31c0a56eaa
Yeshua Colon <yeshuax17@gmail.com>
2014-08-23 22:59:31 -0400

    Update HAL.cpp

--- Kernel/HAL.cpp
+++ Kernel/HAL.cpp
@@ -12,3 +12,24 @@ unsigned char inb(unsigned short port)
     asm volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
     return ret;
 }
+void pushall()//push all registers
+{
+	asm volatile("pushq %rax");
+	asm volatile("pushq %rbx");
+	asm volatile("pushq %rcx");
+	asm volatile("pushq %rdx");
+	asm volatile("pushq %rsi");
+	asm volatile("pushq %rdi");
+	asm volatile("pushq %rbp");	
+
+}
+void popall()//pop all registers
+{
+	asm volatile("popq %rbp");
+	asm volatile("popq %rdi");
+	asm volatile("popq %rsi");
+	asm volatile("popq %rdx");
+	asm volatile("popq %rcx");
+	asm volatile("popq %rbx");
+	asm volatile("popq %rax");
+}
